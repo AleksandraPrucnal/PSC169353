075eb7cf016010c2dbd26ce240a77ff7acef5119
AleksandraPrucnal <115213334+AleksandraPrucnal@users.noreply.github.com>
2023-03-04 16:14:13 +0100

    Update main.c

--- 1_4_10/main.c
+++ 1_4_10/main.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    int n;
+    int n,p = 1;
     printf("Podaj nieujemna liczbe n: ");
     scanf("%i", &n);
 
@@ -11,9 +11,10 @@ int main()
     {
         if(i*i > n)
         {
+            p = i- 1
             break;
         }
     }
-    printf("Pierwiastek zaokraglony w dol z podanej liczby wynosi: %i", )
+    printf("Pierwiastek zaokraglony w dol z podanej liczby wynosi: %i", p);
     return 0;
 }
