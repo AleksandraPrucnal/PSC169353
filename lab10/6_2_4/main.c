9eddd20ff6f44d68d552d480295ad3d968273261
AleksandraPrucnal <115213334+AleksandraPrucnal@users.noreply.github.com>
2023-05-04 15:44:58 +0200

    lab11

--- lab10/6_2_4/main.c
+++ lab10/6_2_4/main.c
@@ -7,15 +7,33 @@ void foo(int**tab, int n, int m)
     for(int i=0; i<n; i++)
     {
         temp = tab[i][m-1];
-        for(int j=m; j<m; j++)
+        for(int j=m-1; j>0; j--)
         {
-            printf("")
+            tab[i][j] = tab[i][j-1];
         }
+        tab[i][0] = temp;
     }
+}
+
+void wyswietl(int **tab, int n, int m)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            printf("[%d][%d]=[%d]", i,j,tab[i][j]);
+        }
+        printf("\n");
+    }
+    printf("----------\n");
 }
 
 int main()
 {
     int ** tab= malloc(2*sizeof(int*));
+    tab[0] = malloc(4*sizeof(int));
+    tab[0] = malloc(4*sizeof(int));
+    tab[0][0] = -3;
+    tab[0][1] = 4;
     return 0;
 }
