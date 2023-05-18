9e6027d62dfbee6f4085786ab80ad6ae333094a8
AleksandraPrucnal <115213334+AleksandraPrucnal@users.noreply.github.com>
2023-05-18 16:10:33 +0200

    lab13

---
+++ lab13/7_3_17/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct element
+{
+    int i;
+    struct element * next;
+};
+
+struct element * usun(struct element * Lista, int a)
+{
+
+};
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
